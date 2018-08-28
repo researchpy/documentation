ca9fec4ed12a68f7f405e9d19af41fed9e9a093c
researchpy <42772889+researchpy@users.noreply.github.com>
2018-08-28 10:37:27 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -74,7 +74,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 
 # -- Options for HTML output -------------------------------------------------
