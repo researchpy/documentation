a11255c7a7b03c666e211ca76ab4b489c68abc57
Corey-Bryant <coreybryant10@gmail.com>
2021-05-10 16:58:33 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018-2021, Corey Bryant'
 author = 'Corey Bryant'
 
 # The short X.Y version
-version = '0.3'
+version = '0.3.1'
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
