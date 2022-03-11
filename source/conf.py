9a25f7870745b8d54042da2d3878865700fe0951
Corey-Bryant <coreybryant10@gmail.com>
2022-03-11 10:37:00 -0500

    Trying custom CSS

--- source/conf.py
+++ source/conf.py
@@ -89,15 +89,16 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-#html_theme_options = {}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-#def setup(app):
-#    app.add_stylesheet("my-styles.css")
+
+def setup(app):
+    app.add_stylesheet("my-styles.css")
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
