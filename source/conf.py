b8f5d0322f459a12788edcc31141e50bff2d8664
Corey Bryant <CoreyBryant10@gmail.com>
2024-02-22 09:59:42 -0500

    Fixed custom css asignment via conf

--- source/conf.py
+++ source/conf.py
@@ -71,7 +71,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -99,10 +99,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'my-styles.css',
+]
 
-
-def setup(app):
-    app.add_stylesheet("my-styles.css")
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
